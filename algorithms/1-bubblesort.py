b6c43a888b4424f00b545ec2820b89bbcd9e229b
Nicolas Huber <info@nicolas-huber.ch>
2024-12-04 08:57:42 +0100

    dev: refactor algorithms

--- algorithms/1-bubblesort.py
+++ algorithms/1-bubblesort.py
@@ -1,14 +1,11 @@
 def bubble_sort_asc(num_list):
     n = len(num_list)
     
-    # Repräsentiert jeden vollständigen Listendurchlauf
-    for i in range(0, n):
+    for i in range(0, n):                   # Repräsentiert jeden vollständigen Listendurchlauf
 
-        # Listendurchlauf von Index 0 bis zum kleinsten unsortierten Element n-i-1
-        for j in range(0, n-i-1):            
+        for j in range(0, n-i-1):           # Listendurchlauf von Index 0 bis zum kleinsten unsortierten Element n-i-1     
 
-            # Element mit nächst grösserem vergleichen und geg. austauschen
-            if num_list[j] > num_list[j+1]:  
+            if num_list[j] > num_list[j+1]: # Element mit nächst grösserem vergleichen und geg. austauschen
                 num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
 
     return num_list                    
