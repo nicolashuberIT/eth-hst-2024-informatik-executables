3b90f35eb0149e395439f2bc7293059617a29c1a
Nicolas Huber <info@nicolas-huber.ch>
2024-12-04 09:09:14 +0100

    dev: refactor algorithms

---
+++ algorithms/search.py
@@ -0,0 +1,35 @@
+# Linear Search & Binary Search in Python
+
+def linear_search(num_list, target):
+  n = len(num_list)
+
+  for i in range(0, n):                               # Durchlauf der gesamten Liste
+    if num_list[i] == target:                         # Überprüfen, ob das aktuelle Element das Ziel ist
+      return i                                        # Ziel gefunden, Index zurückgeben
+
+  return -1                                           # Ziel nicht gefunden
+
+def binary_search(num_list, target):
+    left, right = 0, len(num_list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if num_list[mid] == target:                   # Überprüfen, ob das mittlere Element das Ziel ist
+            return mid                                # Ziel gefunden, Index zurückgeben
+
+        elif num_list[mid] < target:                  # Wenn das Ziel größer ist, ignorieren wir die linke Hälfte
+            left = mid + 1
+
+        else:                                         # Wenn das Ziel kleiner ist, ignorieren wir die rechte Hälfte
+            right = mid - 1
+
+    return -1                                         # Ziel nicht gefunden
+
+# Anwendung
+
+target = 25
+num_list = [64, 34, 25, 12]                            # Numerische Liste (unsortiert)
+num_list_sorted = [12, 25, 34, 64]                     # Sortierte numerische Liste (wichtig für die binäre Suche)
+print(linear_search(num_list, target))                 # >>> 2
+print(binary_search(num_list_sorted, target))          # >>> 1
---
+++ algorithms/sort.py
@@ -0,0 +1,31 @@
+# Bubble Sort in Python
+
+def bubble_sort_asc(num_list):
+    n = len(num_list)
+    
+    for i in range(0, n):                       # Repräsentiert jeden vollständigen Listendurchlauf
+
+        for j in range(0, n-i-1):               # Listendurchlauf von Index 0 bis zum kleinsten unsortierten Element n-i-1     
+
+            if num_list[j] > num_list[j+1]:     # Element mit nächst grösserem vergleichen und geg. austauschen
+                num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
+
+    return num_list                    
+
+def bubble_sort_desc(num_list):
+    n = len(num_list)
+    
+    for i in range(0, n):
+        for j in range(0, n-i-1):
+
+            if num_list[j] < num_list[j+1]:     # einziger Unterschied zu bubble_sort_asc
+                num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
+
+    return num_list
+
+# Anwendung
+
+unsorted_list = [64, 34, 25, 12]                # Unsortierte numerische Liste
+print(bubble_sort_asc(unsorted_list))           # >>> [11, 12, 22, 25, 34, 64, 90]
+print(bubble_sort_desc(unsorted_list))          # >>> [90, 64, 34, 25, 22, 12, 11]
+
